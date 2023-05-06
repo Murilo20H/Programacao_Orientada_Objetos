23b0cc605e97c561cde080135910a5c488496c81
Murilo20H <124621314+Murilo20H@users.noreply.github.com>
2023-05-06 12:40:19 -0300

    Delete Listas/Lista_6 directory

--- Listas/Lista_6/Lista6_exercicio1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Lista 6: exercício 1");
-        Carro carro = new Carro();
-        carro.utilizarHabilidadeEspecial();
-        Moto moto = new Moto();
-        moto.utilizarHabilidadeEspecial();
-    }
-}
