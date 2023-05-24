4eeb0ebcf02df15baad9d180e339d7bd3bed31cf
Murilo20H <murilohenrique123correa@gmail.com>
2023-05-23 21:18:25 -0300

    Listas e aulas

--- Listas/Lista_8/ex_01/src/NomeRepetididoException.java
+++
@@ -1,9 +0,0 @@
-public class NomeRepetididoException extends Exception {
-
-    public NomeRepetididoException() {
-    }
-
-    public NomeRepetididoException(String message) {
-        super(message);
-    }
-}
